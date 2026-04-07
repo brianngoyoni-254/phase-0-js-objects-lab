e8d750a77f8fbea03562da9c22eb956cc65dedd1
Brian Ngoyoni <brianngoyoni254@gmail.com>
2026-04-07 20:31:49 +0300

    chore:updated the index.js file

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 //Write your code here
+// Declaring and initializing the attendee object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
+
+// Function to log attendee's name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+// Function to log ticket price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+// Function to update ticket type
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+// Function to update ticket price
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+// Function to remove the event property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Function to add a checked-in property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
+
 
 
 
